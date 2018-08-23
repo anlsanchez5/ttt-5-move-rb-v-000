939014fe63c0fd428d3b270e6be963948fc5516f
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-08-23 18:38:35 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,11 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+
 def input_to_index(number)
   integer = number.to_i 
   index = integer - 1 
-
 end
 
 def move(board, index, char = "X")
